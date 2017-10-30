fbb7240d812905ac8dee21718ec93668d63d5c4f
CF15004-prn335 <31103005+CF15004-prn335@users.noreply.github.com>
2017-10-30 10:45:27 -0600

    commit inicial

---
+++ src/main/java/reglas/TipoSeccionFacadeLocal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reglas;
+
+import java.util.List;
+import javax.ejb.Local;
+import sv.edu.uesocc.ingenieria.prn335_2017.guia06.TipoSeccion;
+
+/**
+ *
+ * @author Castro
+ */
+@Local
+public interface TipoSeccionFacadeLocal extends BeansFacadeLocal<TipoSeccion> {
+
+    
+}
